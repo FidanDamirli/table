00f81dc4cae66b87e669ea31de2f8a9e3bea0873
Fidan <fidan@MacBook-Pro-Fidan.local>
2023-06-11 00:35:50 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function Table() {
+    var value = document.getElementById("slct").value;
+    var tableHTML = "";
+
+    for (var i = 1; i <= value; i++) {
+
+        for (var j = 1; j <= value; j++) {
+            var cells = "";
+            if (i === j) {
+                cells = "black";
+            }
+
+        }
+    }
+
+}
